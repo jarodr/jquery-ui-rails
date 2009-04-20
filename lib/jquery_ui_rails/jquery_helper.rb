b2665c242d49ac12a3a1a882ae9eb92c4eeb6b9f
Jarod <jarodr@decho.com>
2009-04-20 13:31:40 -0600

    fix document ready

--- lib/jquery_ui_rails/jquery_helper.rb
+++ lib/jquery_ui_rails/jquery_helper.rb
@@ -2,18 +2,17 @@ module JQueryHelper
 
   # renders $(document).ready call back
   def render_jquery_document_ready
-    @jquery_document_ready = [] unless @jquery_document_ready
     %Q{
       $(document).ready(function() {
-        #{@jquery_document_ready.join("\n")}
+        #{flash.delete(:jquery_document_ready).join("\n")}
       })
     }
   end
 
   # adds string to document ready
   def jquery_document_ready
-    @jquery_document_ready = [] unless @jquery_document_ready
-    @jquery_document_ready
+    flash[:jquery_document_ready] = [] unless flash[:jquery_document_ready]
+    flash[:jquery_document_ready]
   end
 
 end
